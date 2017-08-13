c4562145fc643cc7ba8f8bb17f3f549c4ad110e8
Lucas Thompson <dev@lucas.im>
2017-08-13 18:11:44 +0100

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+module.exports = {
+	entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'pyin.umd.js',
+    library: "pYIN",
+		libraryTarget: "umd"
+	},
+	plugins: [
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				worker: {
+					output: {
+						filename: "pyin.worker.js",
+						chunkFilename: "[id].pyin.worker.js"
+					}
+				}
+			}
+		}),
+		new UglifyJSPlugin(
+      {
+        sourceMap: false,
+        uglifyOptions: { mangle: false } // seems there is a bug otherwise
+      }
+    )
+	]
+};
