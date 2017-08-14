f76cb09d30ac430436c671d1edc0c3a07f3043be
Lucas Thompson <dev@lucas.im>
2017-08-14 16:32:20 +0100

    Update to api change, provide a factory function

--- worker-server.js
+++ worker-server.js
@@ -2,8 +2,7 @@ import {WebWorkerServer} from 'piper-js/web-worker';
 import {EmscriptenSynchronousService, EmscriptenModule} from 'piper-js/Emscripten';
 import * as PYinModule from './pyin.asm.umd';
 
-// importScripts('pyin.asm.umd.js'); // Provides PYinModule, without bundling it with webpack
 new WebWorkerServer(
   self,
-  new EmscriptenSynchronousService(PYinModule())
+  () => new EmscriptenSynchronousService(PYinModule())
 );
